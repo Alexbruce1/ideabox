1d0debfe49295747f6275a7117ddd87a7d489e11
benjaminpaige <benjamin.paige@gmail.com>
2018-05-21 12:45:15 -0600

    Changed from using array to storing objects in local storage seperately

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var $body = $('#body-input');
 var $submit = $('.submit-button');
 var ideaArray = [];
 var articleSection = $('article');
-$('article').on('click', '.delete-button', logSomething);
+var parsedIdeaList;
+var thisObjId;
+var theArray;
+var idea;
+$('article').on('click', '.delete-button', deleteIdea);
 $('article').on('click', '.upvote-button', logSomething);
 $('article').on('click', '.downvote-button', logSomething);
 
@@ -11,58 +15,91 @@ $submit.on('click', makeIdea);
 
   // constructor function for ideas:
 
-  function Idea(title,body) {
+  function Idea(title,body,id) {
       this.title = title;
       this.body = body;
-      this.quality = 0;
+      this.qualVal = 0;
+      this.quality = 'Swill';
       this.id = Date.now();
   }
 
   function makeIdea(event){
     event.preventDefault();
+    //using the constructor function to create a new object
     var userIdea = new Idea($title.val(),$body.val());
-    ideaArray.push(userIdea);
-    var stringifiedArray = JSON.stringify(ideaArray);
-    localStorage.setItem('key', stringifiedArray);
-    var arrayFromLocalStorage = localStorage.getItem("key");
-    console.log(arrayFromLocalStorage);
-    var parsedIdeaList = jQuery.parseJSON(arrayFromLocalStorage);
-    // console.log(parsedIdeaList);
-    parsedIdeaList.forEach(function(element) {
-      console.log(element);
-      addIdeaToList(parsedIdeaList);
-    });
-    // addIdeaToDom($title.val(),$body.val());
-    // resetForm();
-  }
+    // ideaArray.push(userIdea);
+    // var stringifiedArray = JSON.stringify(ideaArray);
+    // stringifying that idea object with json
+    var stringifiedIdea = JSON.stringify(userIdea);
+    // creating a local variable from the date.now function which will serve as a unique identifier.
+    var id = Date.now();
+    // set that stringified object into local storage with a key set from the date.now method.
+    localStorage.setItem(id, stringifiedIdea);
+    //as soon as we put that item into local storage we get it back
+    var ideaFromLocalStorage = localStorage.getItem(id);
+    //and then we parse it from the json string into js
+    var parsedIdea = jQuery.parseJSON(ideaFromLocalStorage);
+    //and now we want to take that object and post it to the dom
+    addIdeaToDom(parsedIdea);
+    };
 
-  function addIdeaToList(theArray) {
-    var ideaList = '';
-    for (var i = (theArray.length - 1); i >= 0; i--) {
-      ideaList += (`
+    function addIdeaToDom(object){
+      // console.log(object.title);
+      var idea = (`
       <div class="idea-title-header">
-        <h2 contenteditable="true">${theArray[i].title}</h2>
-        <button alt="delete-button" class="delete-button idea-button"></button>
+        <h2 contenteditable="true">${object.title}</h2>
+        <button alt="delete-button" class="delete-button idea-button" data-id="${object.id}"></button>
       </div>
-      <p contenteditable="true">${theArray[i]. body}</p>
+      <p contenteditable="true" data-id="${object.id}">${object.body}</p>
       <div class="voting-buttons">
-        <button class="idea-button upvote-button"></button>
-        <button class="idea-button downvote-button"></button>
-        <h3>quality: <span class="idea-rating">swill</span></h3>
+        <button class="idea-button upvote-button" data-id="${object.id}"></button>
+        <button class="idea-button downvote-button" data-id="${object.id}"></button>
+        <h3>quality: <span class="idea-rating">${object.quality}</span></h3>
       </div>
   `)
-    };
-    $('article').html(ideaList);
-    
-  }
-
-// function resetForm(event) {
-//   event.preventDefault();
-//   $('#title-input').reset();
-// }
-
-  // $('article').on('click', logSomething);
+  console.log(idea);
+  $('article').prepend(idea);
+    }
 
   function logSomething() {
-      console.log('something');
+    console.log('something');
   }
+
+
+  function deleteIdea(element){
+    parsedIdeaList.forEach(function(element) {
+       if (this.id === parsedIdeaList.id) {
+         console.log('yup');
+       }
+       else {
+         console.log('nope');
+       }
+    // var thisObjId = this.id;
+    // var foo = $.inArray(parseInt(thisObjId), ideaArray);
+    console.log(foo);
+    console.log(thisObjId);
+    console.log(ideaArray);
+  })};
+  // function deleteIdea() {
+  //   var thisObjId = this.id;
+  //   var theArray = parsedIdeaList.filter(thisObjId);
+  //     if (obj.id != thisObjId) {
+  //     console.log(theArray);
+  //   };
+  //   console.log(thisObjId);
+
+  // };
+
+  // function deleteIdea() {
+  //   var thisObjId = this.id;
+  //   $(this).closest('article').remove('article');
+  //   var updatedList = ideaArray.filter(function (obj) {
+  //     return obj.id != thisObjId;
+  //     console.log(obj.id);
+  //   });
+  //   parsedIdeaList = updatedList;
+  //   console.log(ideaArray);
+    // var listItemsStringed = JSON.stringify(linkedList);
+    // localStorage.setItem('list', listItemsStringed);
+    // updateTally();
+  // }
