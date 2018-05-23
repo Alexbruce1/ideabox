49bb6aaee12a2da3be10074bcc9cb3d45933fa20
benjaminpaige <benjamin.paige@gmail.com>
2018-05-22 18:03:11 -0600

    Add some array prototypes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var $title = $('#title-input');
 var $body = $('#body-input');
 var $submit = $('.submit-button');
 var ideaArray = [];
+var blankArray =[];
 var articleSection = $('article');
 var parsedIdeaList;
 var thisObjId;
@@ -27,24 +28,22 @@ $submit.on('click', makeIdea);
     event.preventDefault();
     //using the constructor function to create a new object
     var userIdea = new Idea($title.val(),$body.val());
-    // ideaArray.push(userIdea);
-    // var stringifiedArray = JSON.stringify(ideaArray);
-    // stringifying that idea object with json
-    var stringifiedIdea = JSON.stringify(userIdea);
-    // creating a local variable from the date.now function which will serve as a unique identifier.
-    var id = Date.now();
+    ideaArray.push(userIdea);
+    var stringifiedArray = JSON.stringify(ideaArray);
     // set that stringified object into local storage with a key set from the date.now method.
-    localStorage.setItem(id, stringifiedIdea);
+    localStorage.setItem('ideas', stringifiedArray);
     //as soon as we put that item into local storage we get it back
-    var ideaFromLocalStorage = localStorage.getItem(id);
+    var arrayFromLocalStorage = localStorage.getItem('ideas');
     //and then we parse it from the json string into js
-    var parsedIdea = jQuery.parseJSON(ideaFromLocalStorage);
+    var parsedArray = jQuery.parseJSON(arrayFromLocalStorage);
     //and now we want to take that object and post it to the dom
-    addIdeaToDom(parsedIdea);
+    addIdeaToDom(parsedArray);
     };
 
-    function addIdeaToDom(object){
+    function addIdeaToDom(parsedArray){
       // console.log(object.title);
+      $('article').html('');
+      parsedArray.forEach(function(object){
       var idea = (`
       <div class="idea-title-header">
         <h2 contenteditable="true">${object.title}</h2>
@@ -56,72 +55,101 @@ $submit.on('click', makeIdea);
         <button class="idea-button downvote-button" data-id="${object.id}"></button>
         <h3>quality: <span class="idea-rating">${object.quality}</span></h3>
       </div>
-  `)
-  console.log(idea);
-  $('article').prepend(idea);
-    }
+      `)
+      // console.log(idea);
+      $('article').prepend(idea);
+    
+  })};
 
   function logSomething() {
     console.log('something');
-  }
-
+  };
 
   function deleteIdea(){
     // console.log($(this).data("id"));
     var thisObjectsDataID = $(this).data("id");
     // console.log(thisObjectsDataID);
-    localStorage.removeItem(thisObjectsDataID);
-    // console.log(objectToDelete);
-    $(this).closest('article').remove('article');
+
+    var arrayFromLocalStorage = localStorage.getItem('ideas');
+    var parsedArray = jQuery.parseJSON(arrayFromLocalStorage);
+    var newArray = parsedArray.filter(function(obj){
+      console.log(parsedArray);
+      console.log(obj.id);
+      return obj.id !== thisObjectsDataID
+    });
+    var newStringifiedArray = JSON.stringify(newArray);
+    // set that stringified object into local storage with a key set from the date.now method.
+    localStorage.setItem('ideas', newStringifiedArray);
+    //as soon as we put that item into local storage we get it back
+    var arrayFromLocalStorage = localStorage.getItem('ideas');
+    //and then we parse it from the json string into js
+    var parsedArray = jQuery.parseJSON(arrayFromLocalStorage);
+    //and now we want to take that object and post it to the dom
+    addIdeaToDom(blankArray);
+    addIdeaToDom(parsedArray);
+    // console.log(thisObjectsDataID);
+    // console.log(newArray);
+    // $(this).closest('article').remove('article');
   }
 
   function upvoteIdea(){
     // console.log($(this).data("id"));
     var thisObjectsDataID = $(this).data("id");
+    // console.log(thisObjectsDataID);
+
+    var arrayFromLocalStorage = localStorage.getItem('ideas');
+    var parsedArray = jQuery.parseJSON(arrayFromLocalStorage);
+    var newArray = parsedArray.map(
+      if (this.id === thisObjectsDataID){
+        if (this.quality === 'Swill'){
+          this.quality = "Plausible";
+      } else if (this.quality === 'Plausible'){
+        this.quality = "Genius";
+      // return obj.id !== thisObjectsDataID
+    }
+    console.log(newArray);
+  );
+
     console.log(thisObjectsDataID);
+
+    //get this item from local storage. 
+    var ideaToUpvote = jQuery.parseJSON(localStorage.getItem(thisObjectsDataID));
+    if (ideaToUpvote.quality === 'Swill'){
+      ideaToUpvote.quality = 'Plausible';
+      $(this).closest('span').val('Plausible');
+      //restringify this object and put it back into local storage
+      // $(this).child('.idea-rating').val('Plausible');
+      $()
+    } else if ( ideaToUpvote.quality === 'Plausible' ){
+    ideaToUpvote.quality = 'Genius';
+    $(this).closest('.idea-rating').val('Plausible');
+    }
+    console.log(ideaToUpvote);
+    //change the value of the objects quality.
+      // if the quality is at genius dont change
+      // if the quality is at plausible => genius.
+      // if the quality is at swill => plausible. 
+
+    // add changed object back to local storage.
+    
+    // make the change in the dom to that object.
+
+  
   }
 
    function downvoteIdea(){
     // console.log($(this).data("id"));
     var thisObjectsDataID = $(this).data("id");
     console.log(thisObjectsDataID);
-  }
+        //get this item from local storage. 
 
+    //change the value of its quality.
+      // if the quality is at swill dont change
+      // if the quality is at plausible => swill.
+      // if the quality is at genius => plausible. 
+    //
 
-
-    // parsedIdeaList.forEach(function(element) {
-    //    if (this.id === parsedIdeaList.id) {
-    //      console.log('yup');
-    //    }
-    //    else {
-    //      console.log('nope');
-    //    }
-    // // var thisObjId = this.id;
-    // // var foo = $.inArray(parseInt(thisObjId), ideaArray);
-    // console.log(foo);
-    // console.log(thisObjId);
-    // console.log(ideaArray);
-  
-  // function deleteIdea() {
-  //   var thisObjId = this.id;
-  //   var theArray = parsedIdeaList.filter(thisObjId);
-  //     if (obj.id != thisObjId) {
-  //     console.log(theArray);
-  //   };
-  //   console.log(thisObjId);
-
-  // };
-
-  // function deleteIdea() {
-  //   var thisObjId = this.id;
-  //   $(this).closest('article').remove('article');
-  //   var updatedList = ideaArray.filter(function (obj) {
-  //     return obj.id != thisObjId;
-  //     console.log(obj.id);
-  //   });
-  //   parsedIdeaList = updatedList;
-  //   console.log(ideaArray);
-    // var listItemsStringed = JSON.stringify(linkedList);
-    // localStorage.setItem('list', listItemsStringed);
-    // updateTally();
-  // }
+    // add changed object back to local storage.
+    
+    // make the change in the dom to that object.
+  };
